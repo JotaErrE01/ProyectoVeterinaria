302351399a88c9e521de13fdef1c51250fa6c141
Jonathan <jruiz967@gmail.com>
2021-05-09 19:53:47 -0500

    Primer commit del proyecto

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/js/app.js',
+    output: {
+        filename: 'app.bundle.js',
+        clean: true
+    },
+    optimization:{
+        minimizer: [
+            new CssMinimizerPlugin(), 
+            new TerserPlugin()
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/css", to: "css/" },
+            ]
+        }),
+    ]
+}
